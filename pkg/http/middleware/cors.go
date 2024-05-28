93c25fbd3cf65fa3f6aa019732b76f1ec7c58559
lms <1359816810@qq.com>
2024-05-28 21:45:05 +0800

    pref(http): 完善跨域中间件

--- pkg/http/middleware/cors.go
+++ pkg/http/middleware/cors.go
@@ -9,15 +9,15 @@ func CORSMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		ctx.Writer.Header().Set("Access-Control-Max-Age", "86400")
-		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "*")
+		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Accept")
+		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "*")
 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if ctx.Request.Method == http.MethodOptions {
 			ctx.AbortWithStatus(200)
+			return
 		}
 
-		ctx.Request.Header.Del("Origin")
 		ctx.Next()
-
 	}
 }
