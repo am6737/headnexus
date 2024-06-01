a35a78b084a07a651cfe56cbf04461f14b38f87a
lms <1359816810@qq.com>
2024-06-01 23:28:26 +0800

    fix(hostRule): 修改删除主机规则后配置未更新

--- app/host/command/delete_host_rule.go
+++ app/host/command/delete_host_rule.go
@@ -3,6 +3,8 @@ package command
 import (
 	"context"
 	"fmt"
+	"github.com/am6737/headnexus/config"
+	"github.com/am6737/headnexus/domain/host/entity"
 	"github.com/am6737/headnexus/infra/persistence"
 	"github.com/am6737/headnexus/pkg/code"
 	"github.com/am6737/headnexus/pkg/decorator"
@@ -33,19 +35,55 @@ type deleteHostRuleHandler struct {
 }
 
 func (h *deleteHostRuleHandler) Handle(ctx context.Context, cmd *DeleteHostRule) error {
-	hs, err := h.repos.HostRepo.Get(ctx, cmd.HostID)
+	host, err := h.repos.HostRepo.Get(ctx, cmd.HostID)
 	if err != nil {
 		h.logger.WithError(err).Errorf("failed to get host")
 		return err
 	}
-	fmt.Println("hs => ", hs)
-	if hs.Owner != cmd.UserID {
+	fmt.Println("host => ", host)
+	if host.Owner != cmd.UserID {
 		return code.Forbidden
 	}
 
 	if err := h.repos.HostRuleRepo.DeleteHostRule(ctx, cmd.HostID, cmd.Rules...); err != nil {
 		h.logger.WithError(err).Errorf("failed to delete host rule")
 		return err
+	}
+
+	rules, err := h.repos.RuleRepo.Find(ctx, cmd.UserID, &entity.RuleFindOptions{
+		HostID: cmd.HostID,
+	})
+	if err != nil {
+		h.logger.WithError(err).Errorf("failed to find rule")
+		return err
+	}
+
+	host.Config.Outbound = make([]config.OutboundRule, 0)
+	host.Config.Inbound = make([]config.InboundRule, 0)
+
+	for _, rule := range rules {
+		if rule.Type == entity.RuleTypeOutbound {
+			host.Config.Outbound = append(host.Config.Outbound, config.OutboundRule{
+				Port:   rule.Port,
+				Proto:  rule.Proto.String(),
+				Host:   rule.Host,
+				Action: rule.Action.String(),
+			})
+		}
+		if rule.Type == entity.RuleTypeInbound {
+			host.Config.Inbound = append(host.Config.Inbound, config.InboundRule{
+				Port:   rule.Port,
+				Proto:  rule.Proto.String(),
+				Host:   rule.Host,
+				Action: rule.Action.String(),
+			})
+		}
+	}
+
+	_, err = h.repos.HostRepo.Update(ctx, host)
+	if err != nil {
+		h.logger.WithError(err).Error("update host failed")
+		return nil
 	}
 
 	return err
